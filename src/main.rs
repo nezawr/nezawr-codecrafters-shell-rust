85bc3ce7150d5cfc9bb265666a0f3ae32d55feb2
Nazar <nezar.ras@gmail.com>
2024-12-03 12:36:28 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::io::{self, Write};
 use std::process;
 
 fn main() {
+    // Define built-in commands
+    let built_in: &[&str] = &["type", "echo", "exit"];
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -13,19 +15,41 @@ fn main() {
         stdin.read_line(&mut input).unwrap();
         let input = input.trim();
 
-        if input == "exit 0" {
-            process::exit(0)
-        }
         if input.is_empty() {
             continue;
         }
 
+        if input == "exit 0" {
+            process::exit(0)
+        }
+
+
         let parts: Vec<&str> = input.split_whitespace().collect();
-        if !parts.is_empty() && parts[0] == "echo" {
-            let message = parts[1..].join(" ");
-            println!("{}", message);
-            continue;
+        let command = parts[0];
+        let args = &parts[1..];
+
+        // Match and handle commands
+        match command {
+            "type" => handle_type(args, built_in),
+            "echo" => handle_echo(args),
+            _ => println!("{}: command not found", command)
         }
-        println!("{}: command not found", input)
-        }
+    }
+}
+
+fn handle_type(args: &[&str], built_in: &[&str]) {
+    if args.is_empty() {
+        println!("type: missing argument");
+        return;
+    }
+    let target = args.join(" ");
+    if built_in.contains(&target.as_str()) {
+        println!("{} is a shell builtin", target);
+    } else {
+        println!("{}: not found", target);
+    }
+}
+
+fn handle_echo(args: &[&str]) {
+    println!("{}", args.join(" "));
 }
