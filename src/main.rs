a580445c8fc85445f77da52e34ebc321a34516b8
Nazar <nezar.ras@gmail.com>
2024-12-03 11:32:37 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
 
 fn main() {
     loop {
@@ -12,8 +13,11 @@ fn main() {
         stdin.read_line(&mut input).unwrap();
         let input = input.trim();
 
+        if input == "exit 0" {
+            process::exit(0)
+        }
         if input.is_empty() {
-            break;
+            continue;
         }
         println!("{}: command not found", input)
         }
